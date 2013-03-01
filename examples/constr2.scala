3787b4256125ec7ff65e26467679fd75132c0672
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2013-03-01 21:45:23 +0100

    add example constr2.scala
    demo of Constraints in Model

---
+++ examples/constr2.scala
@@ -0,0 +1,10 @@
+val m = Model(Frame("fr1") has Constraints(
+  Feature("x").Prio #== 42, 
+  Feature("y").Prio #> 3, 
+  Feature("y").Prio #< Feature("x").Prio ))
+ 
+val m2 = m.impose(m / Frame("fr1") !! Constraints) satisfy
+  
+val s: String = m.toScala  
+val m3 = Model.interpret(s)
+if (m3 == m) println("It works!")  
