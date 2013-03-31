cb6b9073dda5c6cb3521016451511cb38fa9ca95
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2013-03-31 14:33:19 +0200

    clean reqt commands from dbg echo

--- reqt.cmd
+++ reqt.cmd
@@ -1,8 +1,5 @@
 :: put this code in a file called reqt.cmd in a dir appended to path
 @echo OFF
 set _jarfilename=%HOMEDRIVE%%HOMEPATH%\reqT\lib\reqT.jar
-echo Adding %_jarfilename% to CLASSPATH
 set CLASSPATH=%CLASSPATH%;%_jarfilename%
-echo %CLASSPATH%
 call scala %HOMEDRIVE%%HOMEPATH%\reqT\lib\reqT.jar %*
-echo ** Exit reqT at %TIME%
--- reqt.sh
+++ reqt.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 JARFILENAME=~/reqT/lib/reqT.jar
 export CLASSPATH=$CLASSPATH:$JARFILENAME
-echo "Appending $JARFILENAME to CLASSPATH"
-echo "$CLASSPATH"
 scala $JARFILENAME $@
 
