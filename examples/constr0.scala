fc75dd60ac3803cb4b119df3653244e6ee7658ec
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2013-04-05 15:07:13 +0200

    extended constraints example

--- examples/constr0.scala
+++ examples/constr0.scala
@@ -1,1 +1,14 @@
-Seq(Var("x") #>= Var("y"), Var("x")::Interval(1,10))
+// a simple constraint problem
+Seq(
+  Var("x") #>= Var("y"), 
+  Var("x")::Interval(1,10),
+  Var("y")::Interval(2,7)
+).solve(Satisfy)
+
+// constraints as a model element
+val cs = Constraints(Feature("x").Prio #>= Feature("y").Prio)
+val Result(conlusion, solutionList) = cs.satisfy
+
+// constraints in a requirements model
+val m = Model(Stakeholder("user") has cs)
+m.constraints.satisfy
