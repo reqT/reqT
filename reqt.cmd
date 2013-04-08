f0ae0f12cf334eeb646ad76c9a8574c82b47e82f
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2013-04-08 17:59:28 +0200

    fix blanks in file name or reqt.cmd using !!

--- reqt.cmd
+++ reqt.cmd
@@ -1,5 +1,6 @@
 :: put this code in a file called reqt.cmd in a dir appended to path
 @echo OFF
+setlocal enableextensions enabledelayedexpansion
 set _jarfilename=%HOMEDRIVE%%HOMEPATH%\reqT\lib\reqT.jar
 set CLASSPATH=%CLASSPATH%;%_jarfilename%
-call scala %HOMEDRIVE%%HOMEPATH%\reqT\lib\reqT.jar %*
+call scala !%HOMEDRIVE%%HOMEPATH%\reqT\lib\reqT.jar! %*
