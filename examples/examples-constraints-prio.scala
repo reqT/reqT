3ab329cd3a063f9567a34bbc9e260af023aa3baa
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2013-04-13 18:08:51 +0200

    line wrapping

--- examples/examples-constraints-prio.scala
+++ examples/examples-constraints-prio.scala
@@ -1,6 +1,8 @@
 var m = Model(
-  Feature("x") has Spec("A feature specification"),
-  Feature("y") has Spec("Another feature specification"),
+  Feature("x") has 
+    Spec("A feature specification"),
+  Feature("y") has 
+    Spec("Another feature specification"),
   Stakeholder("s") has Constraints(
     Feature("x").Prio #< Feature("y").Prio,
     Feature("y").Prio #> 5,
@@ -9,10 +11,16 @@ var m = Model(
   )
 )
 
-val (maximized, priorities) = m.impose(m.constraints).solve(Maximize(Feature("x").Prio))
+val (maximized, priorities) = 
+  m.impose(m.constraints).
+    solve(Maximize(Feature("x").Prio))
 
 println( 
-  maximized == m ++ Model(Feature("x") has Prio(9), Feature("y") has Prio(10))
+  maximized == m ++ 
+    Model(
+      Feature("x") has Prio(9), 
+      Feature("y") has Prio(10)
+    )
 )  // will print true
   
 
