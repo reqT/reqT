745255b0ab7cb1b86bc6529b24a801553c318415
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2014-02-14 17:52:13 +0100

    add metamodel as Model (dog feeding)

--- src/main/scala/reqTmetameta.scala
+++ src/main/scala/reqTmetameta.scala
@@ -11,35 +11,66 @@
 ** http://opensource.org/licenses/bsd-license.php 
 **************************************************************************/
 package reqT
-package metameta 
 
-//this file contains input to the MetaGen metamodel generator
+package object metameta extends MetaModel
 
-object model extends MetaMetamodel {
-  import scala.collection.immutable.ListMap
-  
-  override val enums = ListMap(
-    "Cardinality" -> List("NoOption", "Zero", "One", "ZeroOrOne", "OneOrMany", "ZeroOrMany")
-  )
-  override val attributes = ListMap(
-    "String" -> List("Gist", "Spec", "Text", "Title"),
-    "Int" -> List("Cost", "Prio"),
-    "Cardinality" -> List("Opt")
-  )
-  override val attributeDefault = ListMap(
-    "String" -> "\"???\"",
-    "Int" -> "-99999999",
-    "Cardinality" -> "NoOption"
-  )
-  override val generalEntities = List("Section", "Item", "Label")
-  override val contextEntities = List("Stakeholder","Product","System", "Subdomain")
-  override val requriementEntities = ListMap(
-    "GeneralReq" -> List("Req", "Idea", "Feature"),
-    "IntentionalReq" -> List("Goal","Wish")
-  )
-  override val defaultRelation = "has"
-  override val moreRelations = List("requires","relatesTo")
+package metameta {
+
+  //this file contains input to the MetaGen metamodel generator
+
+  trait HasModel { def model: Model }
+
+  trait MetaModel extends HasModel {
+    lazy val model = Model(
+      Type("Entity") has (
+        Type("General") has  (Type("Section"), Type("Item"), Type("Label")),
+        Type("Context") has (), 
+        Type("Requirement") has ()
+      ),
+      Type("Attribute") has (
+        Type("String") has (Type("Gist"), Type("Spec"), Type("Text"), Type("Title")), 
+        Type("Int") has (Type("Prio"), Type("Cost")), 
+        Type("Enum") has (
+          Type("Cardinality") has (
+            Type("NoOption"), Type("Zero"), Type("One"), Type("ZeroOrOne"), Type("OneOrMany"), Type("ZeroOrMany")
+          )
+        )
+      ),
+      Type("Relation") has (),
+      Type("defaultValues") has (
+        Type("String") has Val("\"???\""),
+        Type("Int") has Val("-99999999"),
+        Type("Cardinality") has Val("NoOption")      
+      )
+    )   
+  }
+
+  object generate extends  MetaMetamodel {
+    import scala.collection.immutable.ListMap
+    
+    val enums = {
+      val m = model / "Attribute" / "Enum"
+      ListMap((m.topIds).map(id => (id , (m / id).topIds)):_*)
+    }
+
+    override val attributes = ListMap(
+      "String" -> List("Gist", "Spec", "Text", "Title"),
+      "Int" -> List("Cost", "Prio"),
+      "Cardinality" -> List("Opt")
+    )
+    override val attributeDefault = ListMap(
+      "String" -> "\"???\"",
+      "Int" -> "-99999999",
+      "Cardinality" -> "NoOption"
+    )
+    override val generalEntities = List("Section", "Item", "Label")
+    override val contextEntities = List("Stakeholder","Product","System", "Subdomain")
+    override val requriementEntities = ListMap(
+      "GeneralReq" -> List("Req", "Idea", "Feature"),
+      "IntentionalReq" -> List("Goal","Wish")
+    )
+    override val defaultRelation = "has"
+    override val moreRelations = List("requires","relatesTo")
+  }
 }
-
-
 
