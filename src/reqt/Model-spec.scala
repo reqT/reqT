6187fb581f3c5ed16f19ded2bd32abd93153465c
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2014-02-23 18:10:30 +0100

    delete bad new line

--- src/reqt/Model-spec.scala
+++ src/reqt/Model-spec.scala
@@ -3,11 +3,9 @@
   "Add empty"          .test {   
     Model() ++ Model() <==> Model()
   } +
-
   "Add same attribute" .test {   
     Model(Spec("a")) ++ Model(Spec("b")) <==> Model(Spec("b")) 
   } +
-
   "Add same entity"    .test { 
     Model(Ent("a")) ++ Model(Ent("b")) <==> Model(Ent("a"),Ent("b")) 
   }  
