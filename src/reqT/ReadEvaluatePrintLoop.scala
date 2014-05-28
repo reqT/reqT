2985a32fe6211341b0a5b3c896d86292e6c7ba89
bjornregnell <bjorn.regnell@cs.lth.se>
2014-05-28 21:02:12 +0200

    fix regression in Scala 2.11 on scala.tools.nsc.interpreter
    loop was made final and could no longer be extended. Found a new way by
    overriding createInterpreter. No doc is available in scaladoc :(

--- src/reqT/ReadEvaluatePrintLoop.scala
+++ src/reqT/ReadEvaluatePrintLoop.scala
@@ -53,57 +53,48 @@ object repl {
         j += 1
       }    
   */
-    interpreter.map { i => 
-      i.intp.reset
-      i.initReqT
-    }
+    reqT.initInterpreter()
+    // interpreter.map { i => 
+      // i.intp.reset
+      // i.initReqT
+    // }
   }
   
   class ReqTILoop(out : PrintWriter) extends ILoop(None, out) {
-    override val prompt = "\nreqT> "
-    override def loop() {
-     if (isAsync) awaitInitialized()
-     initReqT()
-     super.loop()
+    override def createInterpreter() {
+      super.createInterpreter()
+      initReqT()
     }
+    override val prompt = "\nreqT> "
     def initReqT() {
-     intp.quietBind("$intp", intp) //check if this is really needed??
-     intp.quietRun("reqT.initInterpreter($intp)")
+      intp.quietRun("import scala.language._")
+      intp.quietRun("import reqT._")
     }
     override def helpCommand(line: String): Result = {
       if (line == "") echo(helpOnReqT)
       super.helpCommand(line)
     }
 
-     override def printWelcome(): Unit = {
-       out.println(startMsg)
-       out.flush()
-     }
+    override def printWelcome(): Unit = {
+     out.println(startMsg)
+     out.flush()
+    }
   }
   
   class FileRunner(out : PrintWriter, fileName: String) extends ReqTILoop(out : PrintWriter) {
-    override def loop() {
-     if (isAsync) awaitInitialized() 
-     intp.quietBind("$intp", intp) //check if this is really needed??
-     intp.quietRun("reqT.initInterpreter($intp)")
-     intp.quietRun(reqT.load(fileName))
-    }
-     override def printWelcome(): Unit = {
-       //out.println("** reqT FileRunner starting ...")
-       out.flush()
+     override def createInterpreter() {
+      super.createInterpreter()
+      intp.quietRun(reqT.load(fileName)) 
      }
+     override def printWelcome(): Unit = { out.flush() }
   }
   
   class CodeRunner(out : PrintWriter, code: String) extends ReqTILoop(out : PrintWriter) {
-    override def loop() {
-     if (isAsync) awaitInitialized() 
-     intp.quietBind("$intp", intp) //check if this is really needed??
-     intp.quietRun("reqT.initInterpreter($intp)")
-     intp.quietRun(s"{$code}")
+    override def createInterpreter() {
+      super.createInterpreter()
+      intp.quietRun(s"{$code}") 
     }
-     override def printWelcome(): Unit = {
-       out.flush()
-     }
+    override def printWelcome(): Unit = { out.flush() }
   }
 
   def startInterpreting() {
@@ -111,7 +102,7 @@ object repl {
     val settings = new GenericRunnerSettings(out.println)
     settings.usejavacp.value = true
     interpreter = Some( new ReqTILoop(out) )
-    interpreter.map(_.process(settings))
+    interpreter.map(_.process(settings))    
   }
   
   def interpretFile(fileName: String) {
@@ -127,9 +118,8 @@ object repl {
     val settings = new GenericRunnerSettings(out.println)
     settings.usejavacp.value = true
     interpreter = Some( new CodeRunner(out, code) )
-    interpreter.map(_.process(settings))      
+    interpreter.map(_.process(settings))
   }
-  
   
   def quietRun(code: String) { 
     checkIntp() 
@@ -167,53 +157,5 @@ object repl {
 
   def interpretTransformer(code: String): Option[Model => Model] =  
     interpret(s"""{$code}: reqT.Model => reqT.Model""").map(_.asInstanceOf[Model => Model])
-    
-  // def interpretString(code: String): Option[String] = {
-    // checkIntp() 
-    // interpreter .map { i =>
-          // val result = Array[String]("ERROR")
-          // i.beQuietDuring(i.bind("result", "Array[String]", result))
-          // val verdict = i.quietRun(s"""result(0) = $code""")
-          // if (verdict == scala.tools.nsc.interpreter.IR.Success)
-            // Some(result(0))
-          // else None          
-    // } .getOrElse(None)  }
-
-  // def interpretBoolean(code: String): Option[Boolean] = {
-    // checkIntp() 
-    // interpreter .map { i =>
-          // val result = Array[Boolean](false)
-          // i.beQuietDuring(i.bind("result", "Array[Boolean]", result))
-          // val verdict = i.quietRun(s"""result(0) = $code""")
-          // if (verdict == scala.tools.nsc.interpreter.IR.Success)
-            // Some(result(0))
-          // else None          
-    // } .getOrElse(None)  }
-    
-  // def interpretOrElse[T](code: String, orElse: T)( implicit m: scala.reflect.ClassTag[T]): T =  
-    // interpret(code).map(r => //gotta trick erasure
-      // try { m.runtimeClass.cast(r).asInstanceOf[T] } catch { case t: Throwable => orElse } 
-    // ) .getOrElse(orElse)  
-  /* 
-    why does this not work:
-    reqT> repl.interpretOrElse("true",false)
-    res9: Boolean = false
-    when this works:
-    reqT> repl.interpretOrElse(""" "12" """, "13")
-    res5: String = 12
-  */
-
   
-  
-  // def interpretModel(code: String): Option[Model] = { 
-    // checkIntp() 
-    // interpreter .map { i =>
-          // val result = Array[Model](Model())
-          // i.beQuietDuring(i.bind("result", "Array[reqT.Model]", result))
-          // val verdict = i.quietRun(s"""result(0) = $code""")
-          // if (verdict == scala.tools.nsc.interpreter.IR.Success)
-            // Some(result(0)) 
-          // else None          
-    // } .getOrElse(None)
-  // }
 }
