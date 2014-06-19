26b34d8befa32981a8b412cd742d5573c3e7b19e
bjornregnell <bjorn.regnell@cs.lth.se>
2014-06-19 22:51:25 +0200

    add release planning model

--- resources/templates.scala
+++ resources/templates.scala
@@ -60,55 +60,73 @@ Model(
     VariationPoint("shape") binds (Variant("round"), Variant("square"))),
   Product("free") has Configuration("cheap"),
   Product("premium") has Configuration("expensive"))
-//Release planning
+//Release planning simple
+val m = Model(
+  Stakeholder("X") has (
+    Prio(1),
+    Feature("1") has Benefit(4),
+    Feature("2") has Benefit(2),
+    Feature("3") has Benefit(1)),
+  Stakeholder("Y") has (
+    Prio(2),
+    Feature("1") has Benefit(2),
+    Feature("2") has Benefit(1),
+    Feature("3") has Benefit(1)),
+  Release("A") has Order(1),
+  Release("B") has Order(2),  
+  Resource("dev") has (
+    Feature("1") has Cost(10),
+    Feature("2") has Cost(70),
+    Feature("3") has Cost(40),
+    Release("A") has Capacity(100),
+    Release("B") has Capacity(100)),
+  Resource("test") has (
+    Feature("1") has Cost(40),
+    Feature("2") has Cost(10),
+    Feature("3") has Cost(70),
+    Release("A") has Capacity(100),
+    Release("B") has Capacity(100)))
+(m ++ solving.releasePlanningConstraints(m)).maximize(Release("A")/Benefit)    
+//Release planning elaborated
 Model(
-  Feature("autoSave") has Gist("Save changes automatically at regular intervals."),
-  Feature("createPrio") has Gist("Easy entering of feature priorities by stakeholders."),
-  Feature("editAttribute") has Gist("Attributes such as Gist or Description can be edited in the view."),
-  Feature("editImportance") has Gist("The importance of a stakeholder's opinions should be editable."),
-  Feature("editPrio") has Gist("A stakeholder's prioritisation of a requirement should be editable."),
-  Feature("editRelations") has Gist("Relations of entities can be edited."),
-  Feature("listView") has Gist("Requirements can be viewed as a list."),
-  Feature("showUnsaved") has Gist("Show marker to indicate unsaved model."),
-  Feature("viewPrio") has Gist("A stakeholder's prioritisation of requirement can be viewed."),
-  Resource("Team A") has (
+  Feature("autoSave") has Gist("Save a model automatically after each update."),
+  Feature("exportGraph") has Gist("Export model to graph for visualization in e.g. GraphViz."),
+  Feature("exportTable") has Gist("Export model to table format for edit in e.g. Excel."),
+  Feature("modelTemplates") has Gist("Provide templates of typical models to inspire modelling."),
+  Feature("releasePlanning") has Gist("Solve release planning problems."),
+  Feature("syntaxColoring") has Gist("Syntax colored editing of models."),
+  Feature("autoCompletion") has Gist("Auto-completion of entity, attribute and relation types."),  
+  Resource("Dev") has (
     Feature("autoSave") has Cost(4),
-    Feature("createPrio") has Cost(9),
-    Feature("editAttribute") has Cost(6),
-    Feature("editImportance") has Cost(7),
-    Feature("editPrio") has Cost(3),
-    Feature("editRelations") has Cost(6),
-    Feature("listView") has Cost(3),
-    Feature("showUnsaved") has Cost(3),
-    Feature("viewPrio") has Cost(6),
-    Release("March") has Capacity(20),
-    Release("July") has Capacity(15)
-  ),
-  Resource("Team B") has (
-    Feature("autoSave") has Cost(5),
-    Feature("createPrio") has Cost(2),
-    Feature("editAttribute") has Cost(7),
-    Feature("editImportance") has Cost(8),
-    Feature("editPrio") has Cost(9),
-    Feature("editRelations") has Cost(2),
-    Feature("listView") has Cost(4),
-    Feature("showUnsaved") has Cost(3),
-    Feature("viewPrio") has Cost(4),
-    Release("March") has Capacity(15),
-    Release("July") has Capacity(15)
-  ),
-  Release("March") has (Prio(1), Order(0)),
-  Release("July") has (Prio(1), Order(1)),
-  Stakeholder("Supervisor") has (
-    Prio(1), 
-    Feature("autoSave") has Prio(7),
-    Feature("createPrio") has Prio(10),
-    Feature("editAttribute") has Prio(9),
-    Feature("editImportance") has Prio(10),
-    Feature("editPrio") has Prio(10),
-    Feature("editRelations") has Prio(3),
-    Feature("listView") has Prio(6),
-    Feature("showUnsaved") has Prio(4),
-    Feature("viewPrio") has Prio(7)
-  )
-)
+    Feature("exportGraph") has Cost(5),
+    Feature("exportTable") has Cost(2),
+    Feature("modelTemplates") has Cost(7),
+    Feature("releasePlanning") has Cost(20),    
+    Feature("syntaxColoring") has Cost(20),
+    Feature("autoCompletion") has Cost(20)),
+  Resource("Test") has (
+    Feature("autoSave") has Cost(1),
+    Feature("exportGraph") has Cost(2),
+    Feature("exportTable") has Cost(2),
+    Feature("modelTemplates") has Cost(3),
+    Feature("releasePlanning") has Cost(8),
+    Feature("syntaxColoring") has Cost(6),    
+    Feature("autoCompletion") has Cost(6)),
+  Release("Alfa") has Order(0),
+  Release("Beta") has Order(1),
+  Stakeholder("Ada") has (Prio(1), 
+      Feature("autoSave") has Benefit(10),
+      Feature("exportGraph") has Benefit(9),
+      Feature("exportTable") has Benefit(10),
+      Feature("modelTemplates") has Benefit(3),
+      Feature("releasePlanning") has Benefit(4),
+      Feature("syntaxColoring") has Benefit(7),    
+      Feature("autoCompletion") has Benefit(8)),
+    Stakeholder("Ben") has (Prio(1), 
+      Feature("autoSave") has Benefit(10),
+      Feature("exportGraph") has Benefit(9),
+      Feature("exportTable") has Benefit(10),
+      Feature("modelTemplates") has Benefit(3),
+      Feature("releasePlanning") has Benefit(4),
+      Feature("syntaxColoring") has Benefit(7),    
+      Feature("autoCompletion") has Benefit(8)))
