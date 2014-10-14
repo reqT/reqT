3bbe3a7bd52c4331ab8f10eb94733fee5d608be5
bjornregnell <bjorn.regnell@cs.lth.se>
2014-10-14 21:48:48 +0200

    add why+spec+example template

--- resources/templates.scala
+++ resources/templates.scala
@@ -1,3 +1,14 @@
+//Model with Feature having Why+Spec+Example
+Model(
+  Feature("navigate") has (
+    Why(
+"Measuring neural response is a bit painful to the  patient. Electrodes must be kept in place ... So both hands should be at the patient during a measurement."),
+    Spec(
+"It shall be possible to perform the commands start, stop, ... with both hands at the patient."),
+    Example(
+"Might be done with mini keyboard (wrist keys), foot pedal, voice recognition, etc.")
+  )
+)
 //Model to exemplify Html export
 Model(
   Title("Test Model"),
