51d4301add84838c38b7c57f86995ca5e91928d9
bjornregnell <bjorn.regnell@cs.lth.se>
2014-11-05 23:13:54 +0100

    add up and down methods for Status updates

--- src/reqT/ModelStatusOps.scala
+++ src/reqT/ModelStatusOps.scala
@@ -21,6 +21,33 @@ trait ModelStatusOps  {
   lazy val start      = transform { case s: Status => s.start }
   lazy val end        = transform { case s: Status => s.end }
   lazy val dead       = transform { case s: Status => s.dead }
-  lazy val initStatus = transform { case r: Requirement => r.has(Status.start) }
-  def initStatus(et: EntityType) = transform { case e: Entity if e.myType == et => e.has(Status.start) }
+  lazy val initStatus = transform { case r: Requirement => r.has(Status.init) }
+
+  def initStatus(id: String) = transform { case e: Entity if e.id == id => e.has(Status.init) }
+  def initStatus(ent: Entity) = transform { case e: Entity if e == ent => e.has(Status.init) }
+  def initStatus(et: EntityType) = transform { case e: Entity if e.myType == et => e.has(Status.init) }
+
+  def up(id: String) = 
+    transform { case Relation(e,l,t) if e.id == id && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).up)) }
+
+  def up(ent: Entity) = 
+    transform { case Relation(e,l,t) if e == ent && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).up)) }
+      
+  def up(et: EntityType) = 
+    transform { case Relation(e,l,t) if e.myType == et && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).up)) }
+
+  def down(id: String) = 
+    transform { case Relation(e,l,t) if e.id == id && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).down)) }
+      
+  def down(ent: Entity) = 
+    transform { case Relation(e,l,t) if e == ent && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).down)) }
+      
+  def down(et: EntityType) = 
+    transform { case Relation(e,l,t) if e.myType == et && t.isDefinedAt(Status) => 
+      Relation(e,l,t+Status(t.access(Status).down)) }
 }
