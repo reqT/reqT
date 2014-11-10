1ce7ed5870e5ea20f71fe2af1ed508987c5d1c2c
bjornregnell <bjorn.regnell@cs.lth.se>
2014-11-10 14:13:14 +0100

    fix bug dot cmd

--- src/reqT/Utils.scala
+++ src/reqT/Utils.scala
@@ -261,7 +261,7 @@ trait SysUtils {
     if (isWindows) Seq("cmd","/C",s"""${cmd.mkString(" ")}""") else cmd
   def runCmd(cmd: Seq[String]): Int = cmd.! 
   def desktopOpen(f: String) = java.awt.Desktop.getDesktop().open( new java.io.File(f))
-  def isDotInstalled: Boolean = runCmd(fixCmd(Seq("dor","-V"))) == 0
+  def isDotInstalled: Boolean = runCmd(fixCmd(Seq("dot","-V"))) == 0
   def dotCmd(fileName: String, format: String = "pdf", layout: String = "dot", moreArgs: Seq[String] = Seq()): Seq[String] = {
     val q = if (isWindows) '"'.toString else "" 
     val cmd = Seq("dot",s"-T$format",s"-K$layout") ++ moreArgs ++
