444ac7373ae6bcd394cc41d36a61e6083c38bd14
bjornregnell <bjorn.regnell@cs.lth.se>
2017-05-17 20:32:42 +0200

    update Main remove deprecated procedure syntax

--- src/reqT/Main.scala
+++ src/reqT/Main.scala
@@ -1,21 +1,21 @@
-/***     
-**                  _______        
-**                 |__   __|   reqT - a requirements engineering tool  
-**   _ __  ___   __ _ | |      (c) 2011-2014, Lund University  
+/***
+**                  _______
+**                 |__   __|   reqT - a requirements engineering tool
+**   _ __  ___   __ _ | |      (c) 2011-2014, Lund University
 **  |  __|/ _ \ / _  || |      http://reqT.org
-**  | |  |  __/| (_| || |   
-**  |_|   \___| \__  ||_|   
-**                 | |      
-**                 |_|      
-** reqT is open source, licensed under the BSD 2-clause license: 
-** http://opensource.org/licenses/bsd-license.php 
+**  | |  |  __/| (_| || |
+**  |_|   \___| \__  ||_|
+**                 | |
+**                 |_|
+** reqT is open source, licensed under the BSD 2-clause license:
+** http://opensource.org/licenses/bsd-license.php
 **************************************************************************/
 
 package reqT
 
 object Main {
 
-  def help() {
+  def help(): Unit = {
     println("reqT is a requriements engineering tool, visit http://reqT.org")
     println("<no arg> Start the reqT shell inside the Scala Read-Evaluate-Print-Loop")
     println("help     -h  Print this message")
@@ -35,15 +35,15 @@ object Main {
       case a if Set("flex", "-f")(a) => genJFlex(args.drop(1))
       case a if Set("meta", "-m")(a) => genMeta(args.drop(1))
       case a if Set("test", "-t")(a) => test(args.drop(1))
-      case _ => 
+      case _ =>
         println("ERROR Unknown arg: " + args.mkString(" ")); help()
     }
   }
-  
-  def genMeta(args : Array[String]) {
+
+  def genMeta(args : Array[String]): Unit = {
         println(s"Generating metamodel ${args.mkString(",")} ...")
         if (args.isEmpty) reqT.meta.gen().save("GENERATED-metamodel.scala")
-        else repl.initInterpreterAndRun { 
+        else repl.initInterpreterAndRun {
           val q3 = "\"\"\"" // trippel " avoids trouble when backslash in file names
           s"""
             val modelString = load(${q3}${args(0)}${q3})
@@ -54,42 +54,40 @@ object Main {
           """
         }
   }
-  
-  def interpretFile(args : Array[String]) {
+
+  def interpretFile(args : Array[String]): Unit = {
     if (args.isEmpty) println("ERROR no file; usage reqT -i <filename>")
-    args.foreach { s => println(s"interpreting file: $s "); repl.interpretFile(s) } 
+    args.foreach { s => println(s"interpreting file: $s "); repl.interpretFile(s) }
     println("")
   }
 
-  def test(args : Array[String]) {
-    println("** reqT firing up ...") 
+  def test(args : Array[String]): Unit = {
+    println("** reqT firing up ...")
     if (args.isEmpty) println("ERROR no file; usage reqT -i <filename>")
     repl.initInterpreterAndRun(s"""
       val modelFileNames: Vector[String] = Vector(${args.map( f => f.toScala ).mkString(",")})
       println("** Test model files:  " + modelFileNames.mkString(","))
-      modelFileNames .map( f => 
+      modelFileNames .map( f =>
         repl.interpretModel(load(f)) .getOrElse { throw new Error(
-          "INTERPRETATION ERROR! script must evaluate to valid reqT.Model" 
-          ) ; Model() 
+          "INTERPRETATION ERROR! script must evaluate to valid reqT.Model"
+          ) ; Model()
         }
       ) .foreach ( _.test )
-    """) 
+    """)
   }
-  
-  def genJFlex(args : Array[String]) {
-  
+
+  def genJFlex(args : Array[String]): Unit = {
+
     val ent = reqT.metamodel.entityTypes.map(e => s""" "$e" """).
-      mkString(" /* Entity Types */ \n", "| \n", "{ addToken(Token.DATA_TYPE); } \n")    
-    
+      mkString(" /* Entity Types */ \n", "| \n", "{ addToken(Token.DATA_TYPE); } \n")
+
     val attr = reqT.metamodel.attributeTypes.map(e => s""" "$e" """).
       mkString(" /* Attribute Types */ \n", "| \n", "{ addToken(Token.RESERVED_WORD_2); } \n")
-    
+
     val rel = reqT.metamodel.relationTypes.map(e => s""" "$e" """).
       mkString(" /* Relation Types */ \n", "| \n", "{ addToken(Token.FUNCTION); } \n")
-    
+
     (s"$ent\n$attr\n$rel").save("reqT-flex-clauses.txt")
   }
-  
 
-  
 }
