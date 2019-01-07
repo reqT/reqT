75a78ddbcaad1fd0f16444498d1fe06e8cf960cb
bjornregnell <bjorn.regnell@cs.lth.se>
2019-01-07 22:16:45 +0100

    bump version

--- src/reqT/GlobalConstants.scala
+++ src/reqT/GlobalConstants.scala
@@ -14,8 +14,8 @@
 package reqT
 
 trait GlobalConstants { //mixed in by package object reqT
-  val reqT_VERSION = "3.1.3"
-  val reqT_BUILD = 562
+  val reqT_VERSION = "3.1.2"
+  val reqT_BUILD = 563
   val SCALA_VERSION = scala.util.Properties.versionString
   lazy val PREAMBLE = """
 **                  _______
