b55caa9f0eb98cb12514ee4b2d6cf6d1b38062e4
bjornregnell <bjorn.regnell@cs.lth.se>
2019-01-19 21:28:19 +0100

    add deployment script

---
+++ publish-jar.sh
@@ -0,0 +1,7 @@
+echo "scp reqT.jar reqt.org@ssh.reqt.org:/www/reqT.jar"
+scp reqT.jar reqt.org@ssh.reqt.org:/www/reqT.jar
+
+echo "scp reqT-v*.jar reqt.org@ssh.reqt.org:/www/reqT.jar"
+scp reqT-v*.jar reqt.org@ssh.reqt.org:/www/reqT.jar
+
+scp reqT.jar tts-bre@cs.e.lth.lu.se:/usr/local/cs/reqt/reqT.jar
