6d2a929607c2f1ac34f54b357bd2dd25efbc07de
bjornregnell <bjorn.regnell@cs.lth.se>
2020-10-10 22:12:13 +0200

    first cut on minimal encoding in Scala 3

--- gist/src/main/scala/Main.scala
+++ gist/src/main/scala/Main.scala
@@ -1,1 +1,31 @@
+export Entity._
+export Attribute._
+
+trait Elem
+case class Model(elems: Elem*)
+
+trait Node extends Elem
+enum Attribute[T] extends Node: 
+  val value: T 
+  case Spec(value: String) extends Attribute[String] 
+  case Prio(value: Int) extends Attribute[Int]
+
+enum Entity extends Node:
+  val id: String
+  case Req(id: String)
+  case Feature(id: String)
+
+enum RelationType:  
+  case has, requires
+
+case class Relation (
+  val entity: Entity,
+  val link: RelationType,
+  val tail: Model,
+) extends Elem
+
+extension [T](e: Entity)
+  def has(es: Elem*): Relation = Relation(e, RelationType.has, Model(es: _*)) 
+  def requires(es: Elem*): Relation = Relation(e, RelationType.has, Model(es: _*)) 
+
 @main def run = println("*** HELLO Scala 3! ***")
