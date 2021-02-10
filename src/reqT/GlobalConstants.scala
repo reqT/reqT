8020a9e896487811870f914422c1d43fc05a838d
bjornregnell <bjorn.regnell@cs.lth.se>
2021-02-10 19:23:07 +0100

    update to scala 2.12.13

--- src/reqT/GlobalConstants.scala
+++ src/reqT/GlobalConstants.scala
@@ -14,8 +14,8 @@
 package reqT
 
 trait GlobalConstants { //mixed in by package object reqT
-  val reqT_VERSION = "3.1.4"
-  val reqT_BUILD = 566
+  val reqT_VERSION = "3.1.5"
+  val reqT_BUILD = 567
   val SCALA_VERSION = scala.util.Properties.versionString
   lazy val PREAMBLE = """
 **                  _______
