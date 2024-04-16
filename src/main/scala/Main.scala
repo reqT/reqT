f77b49c407fb61194337463a64e8e3b79a6899c9
bjornregnell <bjorn.regnell@cs.lth.se>
2024-04-16 13:56:03 +0200

    update Main commands

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,16 @@
 package reqt
 
-import os.exists
+def edit: Unit = edit()
 
-val scalaVersion = "3.4.1"
-
-def edit(args: Array[String]): Unit = 
+def edit(args: String*): Unit = 
     EditorWindow.newWindow()
     SwingPlatform.runInSwingThread:
       println(s"New window started! EditorWindow.nbrWindows=${EditorWindow.nbrWindows}")
 
-def repl(args: Array[String]): Unit =
+
+def repl: Unit = repl()
+
+def repl(args: String*): Unit =
   val result = scala.util.Try {
     val wdJar   = os.pwd/"reqT.jar"
     val homeJar = os.home/"reqT"/"reqT.jar"
@@ -19,18 +20,24 @@ def repl(args: Array[String]): Unit =
       println(s"You need download reqT.jar from $url and place it in ${os.home/"reqT"}")
       sys.exit(1)
 
-    val cmd = Seq[String]("scala-cli", "repl", "-S", scalaVersion, "--jar", jar.toString)
+    val cmd = Seq[String]("scala-cli", "repl", "-S", Main.scalaVersion, "--jar", jar.toString)
     println(s"Running command: ${cmd.mkString(" ")}")
+    println(s"Type 'import reqt.*' for direct access to full api.")
+    println(s"Type 'edit' to open an editor window.")
+    println(s"Type 'help' for more information on how to use reqT.")
     os.proc(cmd).call(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
   }
   if result.isFailure then 
     println(s"$result")
+    println(s"\nInstall scala-cli from here: https://scala-cli.virtuslab.org/install")
     sys.exit(1)
 
 object Main:
+  val scalaVersion = "3.4.1"
+
   def main(args: Array[String]): Unit = 
-    if args.isEmpty || args(0) == "edit" then edit(args) 
-    else if args.lift(0) == Some("repl") then repl(args)
+    if args.isEmpty || args(0) == "edit" then edit(args.toSeq.drop(1)*) 
+    else if args.lift(0) == Some("repl") then repl(args.toSeq.drop(1)*)
     else println(s"Unknown args: ${args.mkString(",")}")
 
 
